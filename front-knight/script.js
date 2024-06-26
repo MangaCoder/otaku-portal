4670121ddb4c844b2d4156e3a5e0ad0a7f33d408
MangaCoder <lurangzai88@gmail.com>
2024-06-26 16:47:36 +0530

    Add files via upload

---
+++ front-knight/script.js
@@ -0,0 +1,23 @@
+var typed = new Typed('.multiple-text', {
+    strings: [
+      'A STEM Student',
+      'Attended BLDEA',
+      'And I am Frontend Developer',
+      
+    ],
+    
+  
+    
+    typeSpeed: 100,
+  
+    backSpeed: 100,
+    backDelay:1000,
+  
+    
+    loop:true
+  
+    
+    
+  });
+
+  
